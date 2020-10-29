cdf13da9c74310408f800f3c4e6edc72a8f0835f
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-10-29 13:45:14 +0200

    Урок 2. Чтение из файла рандомного числа созданого ранее и загузка в другой tv

--- MyApp_2/src/main/java/com/MyApp_2/MainActivity.java
+++ MyApp_2/src/main/java/com/MyApp_2/MainActivity.java
@@ -1,14 +1,64 @@
 package com.MyApp_2;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String FILE_NAME = "data.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSave = findViewById(R.id.buttonRandNumber);
+        buttonSave.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        TextView tvRandNumber = findViewById(R.id.tvRandNumber);
+        Random random = new Random();
+        tvRandNumber.setText(Integer.toString(random.nextInt(1000) + 1));
+
+        String text = tvRandNumber.getText().toString();
+        try {
+            FileOutputStream out = openFileOutput(FILE_NAME,MODE_APPEND);
+            out.write(text.getBytes());
+            out.close();
+            showToast("В файл добавленны данные");
+        } catch (IOException e) {
+            e.printStackTrace();
+            showToast("Ошибка при записи файла");
+        }
+    }
+
+    public void onClickLoadNum(View view) {
+        try {
+            FileInputStream in = openFileInput(FILE_NAME);
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer);
+            in.close();
+            String s = new String(buffer);
+            ((TextView)findViewById(R.id.tvReadNumberFromFile)).setText(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+            showToast("Ошибка при чтении файла!");
+        }
+    }
+
+    private void showToast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
     }
 }
